d47ebcfd9100bfdb549f8331127d1ffbedbe3d0f
Kevin Lai <kevinlaisoftware@gmail.com>
2024-02-10 13:54:18 -0500

    doc: add docstrings for art endpoints

--- server/server/views.py
+++ server/server/views.py
@@ -3,11 +3,26 @@ from .models import Art
 from .serializers import ArtSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.shortcuts import render
 
 
 @api_view(['GET', 'POST'])
 def art_list(request, format=None):
+    """
+    API endpoint for retrieving a list of art or creating a new art.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        Response: The HTTP response object containing the serialized art data.
+
+    Methods:
+        GET: Retrieves a list of art objects.
+        POST: Creates a new art object.
+
+    Raises:
+        N/A
+    """
     if request.method == 'GET':
         art = Art.objects.all()
         serializer = ArtSerializer(art, many=True)
@@ -21,6 +36,25 @@ def art_list(request, format=None):
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def art_detail(request, id, format=None):
+    """
+    Retrieve, update or delete an art object.
+
+    Parameters:
+    - request: The HTTP request object.
+    - id: The ID of the art object to retrieve, update or delete.
+    - format: The format of the response data (optional).
+
+    Returns:
+    - If the request method is GET:
+        - A JSON response containing the serialized art object and the URL of the art image.
+    - If the request method is PUT:
+        - If the serializer is valid, a response with the updated serialized art object.
+        - If the serializer is invalid, a response with the serializer errors and status code 400.
+    - If the request method is DELETE:
+        - A response with status code 204 indicating successful deletion.
+    - If the art object with the specified ID does not exist:
+        - A response with status code 404 indicating not found.
+    """
     try:
         art = Art.objects.get(pk=id)
     except Art.DoesNotExist:
